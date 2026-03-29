1d2e4b4f0346eb3c6162742e343f5ad0fa56bf5d
Yuchera <email3@example.com>
2026-03-29 11:28:25 +0300

    Fix mail attempt lookup and count queries

--- src/Application/Db/MySQL/Repository/MailAttemptRepository.php
+++ src/Application/Db/MySQL/Repository/MailAttemptRepository.php
@@ -13,6 +13,7 @@ use Semitexa\Orm\OrmManager;
 use Semitexa\Orm\Query\Direction;
 use Semitexa\Orm\Query\Operator;
 use Semitexa\Orm\Repository\DomainRepository;
+use Semitexa\Orm\Uuid\Uuid7;
 
 #[SatisfiesRepositoryContract(of: MailAttemptRepositoryInterface::class)]
 class MailAttemptRepository implements MailAttemptRepositoryInterface
@@ -39,14 +40,19 @@ class MailAttemptRepository implements MailAttemptRepositoryInterface
     {
         /** @var list<MailAttemptResource> */
         return $this->repository()->query()
-            ->where(MailAttemptTableModel::column('mail_message_id'), Operator::Equals, $messageId)
+            ->where(MailAttemptTableModel::column('mail_message_id'), Operator::Equals, Uuid7::toBytes($messageId))
             ->orderBy(MailAttemptTableModel::column('attempt_no'), Direction::Asc)
             ->fetchAllAs(MailAttemptResource::class, $this->orm()->getMapperRegistry());
     }
 
     public function countByMessageId(string $messageId): int
     {
-        return count($this->findByMessageId($messageId));
+        $result = $this->adapter()->execute(
+            'SELECT COUNT(*) AS cnt FROM mail_attempts WHERE mail_message_id = :message_id',
+            ['message_id' => Uuid7::toBytes($messageId)],
+        );
+
+        return (int) ($result->rows[0]['cnt'] ?? 0);
     }
 
     private function repository(): DomainRepository
@@ -60,6 +66,11 @@ class MailAttemptRepository implements MailAttemptRepositoryInterface
     private function orm(): OrmManager
     {
         return $this->orm ??= new OrmManager();
+    }
+
+    private function adapter(): \Semitexa\Orm\Adapter\DatabaseAdapterInterface
+    {
+        return $this->orm()->getAdapter();
     }
 
     private function copyIntoMutableResource(object $source, MailAttemptResource $target): void
